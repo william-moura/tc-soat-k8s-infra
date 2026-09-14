cdc60027def203cb3f362e6a29eb830983791857
Will_Moura <william@febrafar.com.br>
2026-09-14 15:03:30 -0300

    ajuste

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "k8s_server" {
-  ami                  = "ami-0c7217cdde317cfec"
+  ami                  = "data.aws_ami.ubuntu.id"
   instance_type        = "t3.small"
   key_name             = "vockey"
   iam_instance_profile = "LabInstanceProfile"
